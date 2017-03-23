faa97dd405ba70a37d1930966fdaf85a78bc8f37
rswong <ziud7rs@gmail.com>
2017-03-23 16:17:23 +0800

    debug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "welcome!"
+    flash[:alert] = "Review carefully!"
+    flash[:warning] = "Please friendly."
+  end
 end
