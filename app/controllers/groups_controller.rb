2ff6861ce08733f836c75c0baa908f05ec664e34
rswong <ziud7rs@gmail.com>
2017-03-23 23:17:56 +0800

    join & quit action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,6 +43,35 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "Saved success!"
+      else
+        flash[:warning] = "Have saved!"
+      end
+
+      redirect_to group_path(@group)
+   end
+
+   def quit
+     @group = Group.find(params[:id])
+
+      if current_user.is_member_of?(@group)
+        current_user.quit!(@group)
+        flash[:alert] = "Unsaved this movie!"
+      else
+        flash[:warning] = "Have not saved!"
+      end
+
+      redirect_to group_path(@group)
+    end
+
+
+
+
   private
 
   def find_group_and_check_permission
