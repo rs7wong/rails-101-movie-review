804643092ac350d34a81edd7f445d270641cbf4c
rswong <ziud7rs@gmail.com>
2017-03-24 13:58:54 +0800

    implement posts#updatedestroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,6 +33,15 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "Review deleted"
+    redirect_to account_posts_path
+  end
+
+
     private
 
     def post_params
